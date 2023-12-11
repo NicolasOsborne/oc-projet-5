b3d6e2c892764a497ebb0de97f819ba097371da0
Nicolas Osborne <nho.osborne@gmail.com>
2023-12-11 16:30:44 +0100

    Etapes 1-2-3 : Ajout des flèches (left & right) en HTML / Création des Event Listeners sur les flèches / Ajout des bulletpoints & fonction currentDot

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+// Tableau avec les images du carousel
+
 const slides = [
   {
     image: 'slide1.jpg',
@@ -17,3 +19,44 @@ const slides = [
     tagLine: 'Autocollants <span>avec découpe laser sur mesure</span>',
   },
 ]
+
+// Event listeners des flèches de défilement
+
+let previousImage = document.querySelector('.arrow_left')
+previousImage.addEventListener('click', () => {
+  console.log('Défiler vers la gauche')
+})
+
+let nextImage = document.querySelector('.arrow_right')
+nextImage.addEventListener('click', () => {
+  console.log('Défiler vers la droite')
+})
+
+// Ajout des bullet-points en fonction du nombre de slides dans le tableau
+
+const dots = document.querySelector('.dots')
+
+let currentSlide = 0
+
+function addBulletpoints(slides) {
+  for (let i = 0; i < slides.length; i++) {
+    let dot = document.createElement('span')
+    dot.classList.add('dot')
+    dots.appendChild(dot)
+  }
+}
+
+function currentDot() {
+  let bulletpoints = document.querySelectorAll('.dot')
+  for (let index = 0; index < bulletpoints.length; index++) {
+    let dot = bulletpoints[index]
+    if (index === currentSlide) {
+      dot.classList.add('dot_selected')
+    }
+  }
+}
+
+addBulletpoints(slides)
+currentDot()
+
+console.log(dots)
